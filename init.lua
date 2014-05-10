a16baa68c867976256b01a5dc21f81168834320b
Randy-Mills <v64ui@unb.ca>
2014-05-10 17:33:38 -0300

    Initial commit. Wonderous items tables

---
+++ init.lua
@@ -0,0 +1,13 @@
+--
+-- SjoCi's dice init file
+--
+
+require("ts3init")
+require("items/events")
+require("items/items_table")
+
+local registeredEvents = {
+	onTextMessageEvent = itemroll_events.onTextMessageEvent
+}
+
+ts3RegisterModule("itemroll", registeredEvents)
